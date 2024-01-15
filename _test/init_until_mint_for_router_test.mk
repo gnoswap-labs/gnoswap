7c9159fa0b2d0f7fcffa0ec292fda2c2a34f7567
n3wbie <r3v4@onbloc.xyz>
2024-01-15 17:36:10 +0900

    GSW-788 feat: setup env for manual testing

---
+++ _test/init_until_mint_for_router_test.mk
@@ -0,0 +1,96 @@
+ADDR_POOL := g15z32w7txv6lw259xzhzzmwtwmcjjc0m6dqzh6f
+ADDR_POS := g10wwa53xgu4397kvzz7akxar9370zjdpwux5th9
+ADDR_STAKER := g1puv9dz470prjshjm9qyg25dyfvrgph2kvjph68
+ADDR_ROUTER := g1pjtpgjpsn4hjfv2n4mpz8cczdn32jkpsqwxuav
+ADDR_GOV := g1kmat25auuqf0h5qvd4q7s707r8let5sky4tr76
+
+
+ADDR_TEST1 := g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5
+
+TX_EXPIRE := 9999999999
+
+MAKEFILE := $(shell realpath $(firstword $(MAKEFILE_LIST)))
+GNOLAND_RPC_URL ?= localhost:26657
+CHAINID ?= dev
+ROOT_DIR:=$(shell dirname $(MAKEFILE))/..
+
+.PHONY: help
+help:
+	@echo "Available make commands:"
+	@cat $(MAKEFILE) | grep '^[a-z][^:]*:' | cut -d: -f1 | sort | sed 's/^/  /'
+
+.PHONY: all
+all: wait deploy approve pool-setup position-setup admin-change done
+
+.PHONY: deploy
+deploy: deploy-grc20s deploy-gnft deploy-gov deploy-pool deploy-position deploy-staker deploy-router deploy-grc20_wrapper
+
+.PHONY: setup
+setup: multi-msg-01
+
+
+wait:
+	$(info ************ [ETC] wait 10 seconds for chain to start ************)
+	$(shell sleep 10)
+
+# Deploy Tokens
+deploy-grc20s:
+	$(info ************ [GRC20] deploy tokens ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/wugnot -pkgpath gno.land/r/demo/wugnot -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/gns -pkgpath gno.land/r/demo/gns -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token01 -pkgpath gno.land/r/demo/token01 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token02 -pkgpath gno.land/r/demo/token02 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token03 -pkgpath gno.land/r/demo/token03 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token04 -pkgpath gno.land/r/demo/token04 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token05 -pkgpath gno.land/r/demo/token05 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token06 -pkgpath gno.land/r/demo/token06 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token07 -pkgpath gno.land/r/demo/token07 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token08 -pkgpath gno.land/r/demo/token08 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token09 -pkgpath gno.land/r/demo/token09 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/token10 -pkgpath gno.land/r/demo/token10 -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+	
+deploy-gnft:
+	$(info ************ [GNFT] deploy lp token ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/gnft -pkgpath gno.land/r/demo/gnft -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+
+# Deploy Contracts
+deploy-gov:
+	$(info ************ [GOV] deploy governance ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/gov -pkgpath gno.land/r/demo/gov -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+
+deploy-pool:
+	$(info ************ [POOL] deploy pool ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/pool -pkgpath gno.land/r/demo/pool -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+
+deploy-position:
+	$(info ************ [POSITION] deploy position ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/position -pkgpath gno.land/r/demo/position -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+
+deploy-staker:
+	$(info ************ [STAKER] deploy staker ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/staker -pkgpath gno.land/r/demo/staker -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+
+deploy-router:
+	$(info ************ [ROUTER] deploy router ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/router -pkgpath gno.land/r/demo/router -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+
+deploy-grc20_wrapper:
+	$(info ************ [GRC20 Wrapper] deploy grc20_wrapper ************)
+	@echo "" | gnokey maketx addpkg -pkgdir $(ROOT_DIR)/_setup/grc20_wrapper -pkgpath gno.land/r/demo/grc20_wrapper -insecure-password-stdin=true -remote $(GNOLAND_RPC_URL) -broadcast=true -chainid $(CHAINID) -gas-fee 1ugnot -gas-wanted 9000000 -memo "" test1 > /dev/null
+	@echo
+
+
+# MULTI MSG UNTIL MINT
+multi-msg-01:
+	$(info ************ [MULTI-MSG] ************)
+	@echo "" | gnokey sign -txpath multi_msg_test1.txt -insecure-password-stdin=true -chainid $(CHAINID) -number 5 -sequence 19 test1 > signed_test1.tx
+	gnokey broadcast -remote $(GNOLAND_RPC_URL) signed_test1.tx > /dev/null
