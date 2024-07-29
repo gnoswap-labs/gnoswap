ee2a6e374dde6500abb236ffd9647b88b71a7ddf
Lee ByeongJun <lbj199874@gmail.com>
2024-07-29 15:03:15 +0900

    auto detection

--- setup.py
+++ setup.py
@@ -3,33 +3,46 @@ import shutil
 import argparse
 import subprocess
 from pathlib import Path
+import re
 
 def clone_repo(workdir):
     os.chdir(workdir)
     subprocess.run(["git", "clone", "https://github.com/gnoswap-labs/gnoswap.git"], check=True)
     os.chdir("gnoswap")
 
+def get_module_path(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    match = re.search(r'module\s+([\w./]+)', content)
+    if match:
+        return match.group(1)
+    return None
+
 def copy_contracts(workdir):
     gno_dir = os.path.join(workdir, "gno", "examples", "gno.land")
 
     # Copy GRC20 tokens
     shutil.copytree("__local/grc20_tokens", os.path.join(gno_dir, "r", "demo"), dirs_exist_ok=True)
-    shutil.copytree("_deploy/r/gnoswap", os.path.join(gno_dir, "r"), dirs_exist_ok=True)
 
-    # Copy gnoswap base packages
-    shutil.copytree("_deploy/p/gnoswap", os.path.join(gno_dir, "p", "gnoswap"), dirs_exist_ok=True)
-
-    # Copy gnoswap base realms
-    shutil.copytree("_deploy/r/gnoswap", os.path.join(gno_dir, "r", "gnoswap"), dirs_exist_ok=True)
+    # Copy gnoswap base packages and realms
+    for root, dirs, files in os.walk("_deploy"):
+        for file in files:
+            if file == "gno.mod":
+                src_dir = os.path.dirname(os.path.join(root, file))
+                module_path = get_module_path(os.path.join(root, file))
+                if module_path:
+                    dest_dir = os.path.join(gno_dir, *module_path.split('/')[1:])
+                    shutil.copytree(src_dir, dest_dir, dirs_exist_ok=True)
 
     # Copy gnoswap realms
-    for realm in ["pool", "position", "router", "staker"]:
-        shutil.copytree(realm, os.path.join(gno_dir, "r", "gnoswap"), dirs_exist_ok=True)
+    # TODO: Detect realms automatically
+    for realm in ["pool", "position", "router", "staker", "emission", "community_pool"]:
+        shutil.copytree(realm, os.path.join(gno_dir, "r", "gnoswap", realm), dirs_exist_ok=True)
 
 def move_tests(workdir):
     gno_dir = os.path.join(workdir, "gno", "examples", "gno.land", "r")
 
-    for realm in ["pool", "position", "router", "staker"]:
+    for realm in ["pool", "position", "router", "staker", "emission", "community_pool"]:
         test_dir = os.path.join(gno_dir, realm, "_TEST_")
         if os.path.exists(test_dir):
             for item in os.listdir(test_dir):
