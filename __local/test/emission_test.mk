d86bc1c53147735949ba60500b6f4e79d44e4cae
n3wbie <r3v4@onbloc.xyz>
2024-08-08 14:14:20 +0900

    build:

--- __local/test/emission_test.mk
+++ __local/test/emission_test.mk
@@ -21,11 +21,11 @@ TX_EXPIRE := 9999999999
 
 MAKEFILE := $(shell realpath $(firstword $(MAKEFILE_LIST)))
 
-# GNOLAND_RPC_URL ?= http://localhost:26657
-# CHAINID ?= dev
+GNOLAND_RPC_URL ?= http://localhost:26657
+CHAINID ?= dev
 
-GNOLAND_RPC_URL ?= https://dev.rpc.gnoswap.io:443
-CHAINID ?= dev.gnoswap
+# GNOLAND_RPC_URL ?= https://dev.rpc.gnoswap.io:443
+# CHAINID ?= dev.gnoswap
 
 ROOT_DIR:=$(shell dirname $(MAKEFILE))/../../
 
