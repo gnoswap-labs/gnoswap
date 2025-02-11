40ffadcfa1e5b67411b662f503e080baa6d8ce99
Lee ByeongJun <lbj199874@gmail.com>
2025-02-11 12:53:36 +0900

    directory auto detect

---
+++ .github/script/generate_matrix.rb
@@ -0,0 +1,25 @@
+#!usr/bin/env ruby
+# .github/script/generate_matrix.rb
+
+require 'json'
+
+# Configuration
+BASE_PATH = 'gno/examples/gno.land'
+PATTERNS = [
+  '*/p/gnoswap/*',
+  '*/r/gnoswap/*'
+]
+
+# Find all matching directories
+folders = Dir.glob("#{BASE_PATH}/**/*")
+  .select { |f| File.directory?(f) }
+  .select { |f| PATTERNS.any? { |p| File.fnmatch(p, f) } }
+
+# Generate matrix data
+matrix = folders.map { |f| {
+  'name' => File.basename(f),
+  'folder' => f
+}}
+
+# Output JSON to stdout
+puts JSON.generate(matrix)
