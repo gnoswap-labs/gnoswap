8c6da1b9673ead059b351cd62df8bd5e41549d27
0xTopaz <60733299+onlyhyde@users.noreply.github.com>
2025-02-20 16:32:19 +0900

    test: pool, position, router contract test (#505)
    * chore : add .idea to gitignore
    
    * test: add script to test in local
    
    * test: update test running script
    
    * test: Updated to enable individual testing
    
    * chore: update test script
    
    * refactor : CreatePool
    
    * fix: When poolPath ordering is false, use the exact current tick
    
    * fix : pool parameter checking bug fix when position mint
    
    1. positionCaller Address validity check
    2. max tick upper range check
    3. test code updated
    
    * refactor: support feeTier
    
    * refactor : support to guard invalid parameter when pool create
    
    * test : add unit test to check newPoolParam type
    
    * refactor : separate pool type
    
    * refactor : Integration of position-related codes
    
    * refactor : Integration of protocol-fee-related codes
    
    * test: pool mint test case added
    
    * test: add test case pool has feeTier100
    
    * chore: remove print log
    
    * test: add collect test case
    
    * chore: Use the previous version before changing the Std prefix
    
    * refactor : remove fee protocol code
    
    * chore: Temporarily for ci operation gno commit change
    
    * chore: rollback
    
    * test: collectProtocol test
    
    * refactor: remove unused code and change token validation check time
    
    * chore: remove unused code and tickupper change not max tick
    
    * chore: remove unused code
    
    * fix: tlin error
    
    * fix : test case error
    
    * fix: tlin error
    
    ---------
    
    Co-authored-by: Lee ByeongJun <lbj199874@gmail.com>

---
+++ scripts/test_values.mk
@@ -0,0 +1,36 @@
+# Matrix configurations
+TEST_KEYS:=p/uint256 \
+	p/int256 \
+	p/gnsmath \
+	r/common \
+	r/gns \
+	r/gnft \
+	r/gov/xgns \
+	r/emission \
+	r/protocol_fee \
+	r/pool \
+	r/position \
+	r/router \
+	r/staker \
+	r/community_pool \
+	r/gov/staker \
+	r/gov/governance \
+	r/launchpad
+
+TEST_VALUES:=gno/examples/gno.land/p/gnoswap/uint256 \
+	   gno/examples/gno.land/p/gnoswap/int256 \
+	   gno/examples/gno.land/p/gnoswap/gnsmath \
+	   gno/examples/gno.land/r/gnoswap/v1/common \
+	   gno/examples/gno.land/r/gnoswap/v1/gns \
+	   gno/examples/gno.land/r/gnoswap/v1/gnft \
+	   gno/examples/gno.land/r/gnoswap/v1/gov/xgns \
+	   gno/examples/gno.land/r/gnoswap/v1/emission \
+	   gno/examples/gno.land/r/gnoswap/v1/protocol_fee \
+	   gno/examples/gno.land/r/gnoswap/v1/pool \
+	   gno/examples/gno.land/r/gnoswap/v1/position \
+	   gno/examples/gno.land/r/gnoswap/v1/router \
+	   gno/examples/gno.land/r/gnoswap/v1/staker \
+	   gno/examples/gno.land/r/gnoswap/v1/community_pool \
+	   gno/examples/gno.land/r/gnoswap/v1/gov/staker \
+	   gno/examples/gno.land/r/gnoswap/v1/gov/governance \
+	   gno/examples/gno.land/r/gnoswap/v1/launchpad
