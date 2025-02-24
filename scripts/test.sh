cb8f66329434c6a4ec39726ef19d5a1d62b36883
0xTopaz <60733299+onlyhyde@users.noreply.github.com>
2025-02-24 18:53:52 +0900

    test: position contract test (#540)
    * refactor: pool contract code clean up and formatting
    
    * refactor: name change to newErrorDetail
    
    * refactor: position contract
    
    * test: modified some tests and add new mint test case
    
    * refactor: use the token balance in the ledger instaed of the calculated value
    
    * refactor : remove unnecessary function calls to reduce gas
    
    * refactor : If zero ungot is warped, panic is processed
    
    * refactor: remove unused code

--- scripts/test.sh
+++ scripts/test.sh
@@ -28,10 +28,7 @@ clone_repos() {
 
   # Clone gno repository into $TMP_PATH
   echo "✅ Cloning gno repository into tmp/gno..."
-  git clone --depth 30 --branch master https://github.com/gnolang/gno.git "$GNO_PATH"
-  cd "$GNO_PATH"
-  git checkout ec696d40ae44eee809e3ca2c7cdb2ce4c6eece04
-  cd "$PROJECT_ROOT"
+  git clone --depth 1 --branch master https://github.com/gnolang/gno.git "$GNO_PATH"
 }
 
 # ✅ env setup(Go, Python install)
