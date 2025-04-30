2512b0ea6401a8d01557fbf00bc73b27a3118019
Lee ByeongJun <lbj199874@gmail.com>
2025-04-30 15:30:08 +0900

    clean up integration dir

--- .github/scripts/run_txtar_tests.rb
+++ .github/scripts/run_txtar_tests.rb
@@ -27,16 +27,19 @@ class TxtarTestRunner
   def copy_txtar_files
     puts "Copying txtar files to integration test directory"
     FileUtils.mkdir_p(@integration_dir)
-    
+
+    puts "Cleaning up existing files in #{@integration_dir}"
+    existing_files = Dir.glob(File.join(@integration_dir, "*.txtar"))
+    existing_files.each do |file|
+      puts "Removing #{file}"
+      FileUtils.rm(file)
+    end
+
     # Find all .txtar files in the source directory
     txtar_files = Dir.glob(File.join(@source_dir, "**", "*.txtar"))
     
     txtar_files.each do |file|
       dest = File.join(@integration_dir, File.basename(file))
-      
-      # Skip if source and destination are the same file
-      next if File.identical?(file, dest)
-      
       puts "Copying #{file} to #{dest}"
       FileUtils.cp(file, dest)
     end
