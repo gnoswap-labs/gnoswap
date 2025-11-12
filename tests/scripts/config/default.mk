e559542aa0dea3f20abd935037d60a0cae3da43f
jinoosss <112360739+jinoosss@users.noreply.github.com>
2025-11-12 13:02:56 +0900

    chore(deploy): manages deployment configurations

--- tests/scripts/config/default.mk
+++ tests/scripts/config/default.mk
@@ -1,7 +1,11 @@
-# r/demo/wugnot from gno
-ADDR_WUGNOT := g15vj5q08amlvyd0nx6zjgcvwq2d0gt9fcchrvum
+# Local Environment Configuration
 
-# based on v1
+# RPC and Chain Configuration
+GNOLAND_RPC_URL ?= localhost:26657
+CHAINID ?= dev
+
+# Contract Addresses
+ADDR_WUGNOT := g15vj5q08amlvyd0nx6zjgcvwq2d0gt9fcchrvum
 ADDR_POOL := g1dexaf6aqkkyr9yfy9d5up69lsn7ra80af34g5v
 ADDR_POSITION := g1y3uyaa63sjxvah2cx3c2usavwvx97kl8m2v7ye
 ADDR_ROUTER := g1vc883gshu5z7ytk5cdynhc8c2dh67pdp4cszkp
@@ -13,23 +17,27 @@ ADDR_LAUNCHPAD := g1x6r75sxlkp9zfqufew0vcuq9sfclsq8uaqkru9
 ADDR_GNS := g13ffa5r3mqfxu3s7ejl02scq9536wt6c2t789dm
 ADDR_GNFT := g1mclfz2dn4lnez0lcjwgz67hh72rdafjmufvfmw
 
-# username address
-ADDR_GNOSWAP :=
-ADDR_ADMIN :=
-ADDR_TEST :=
+# User Addresses (used for test scripts)
+ADDR_GNOSWAP := g1lmvrrrr4er2us84h2732sru76c9zl2nvknha8c
+ADDR_ADMIN := g17290cwvmrapvp869xfnhhawa8sm9edpufzat7d
+ADDR_TEST := g1mjqcxzek8yacgcvnqfkj0dck67wdyhqlfp9unr
 
-# INCENTIVE_START
+# Test User Addresses
+ADDR_TEST_ADMIN := g1lmvrrrr4er2us84h2732sru76c9zl2nvknha8c
+ADDR_USER_1 := # SET ACCOUNTS FOR TESTING
+ADDR_USER_2 := # SET ACCOUNTS FOR TESTING
+ADDR_USER_3 := # SET ACCOUNTS FOR TESTING
+ADDR_USER_4 := # SET ACCOUNTS FOR TESTING
+
+# Incentive Configuration
 TOMORROW_MIDNIGHT := $(shell (gdate -ud 'tomorrow 00:00:00' +%s))
 INCENTIVE_END := $(shell expr $(TOMORROW_MIDNIGHT) + 7776000) # 7776000 SECONDS = 90 DAY
 
-# MAX_UINT64 := 18446744073709551615
+# Transaction Configuration
 MAX_APPROVE := 9223372036854775806
 TX_EXPIRE := 9999999999
 
+# Path Configuration
 MAKEFILE := $(shell realpath $(firstword $(MAKEFILE_LIST)))
-ROOT_DIR:=$(shell dirname $(MAKEFILE))/../
+ROOT_DIR := $(shell cd $(shell dirname $(MAKEFILE))/../.. && pwd)
 
-
-# TODO: change below 2 values based on which chain to deploy
-GNOLAND_RPC_URL ?= localhost:26657
-CHAINID ?= dev
